51ceb95b2bf7c3ecc099038d0914dff11058610c
Dagmar87 <jdfssobrinho@hotmail.com>
2024-10-15 11:41:32 -0300

    Criação do arquivo clinic_noticias.sql

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 global $pdo;
 
 try {
-	$pdo = new PDO("mysql:dbname=clinic-noticias;host=localhost", "root", "");
+	$pdo = new PDO("mysql:dbname=clinic_noticias;host=localhost", "root", "");
 } catch (PDOException $e) {
 	echo "Erro na tentativa de conexão com o banco de dados: " . $e->getMessage();
 	exit;
