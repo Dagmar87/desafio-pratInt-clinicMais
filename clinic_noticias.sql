a3ee785157f863051d15fd20a64bb78e0c841e4a
Dagmar87 <jdfssobrinho@hotmail.com>
2024-10-16 10:57:05 -0300

    Adição do comentario_id nos códigos do arquivo clinic_noticias.sql

--- clinic_noticias.sql
+++ clinic_noticias.sql
@@ -7,18 +7,26 @@ SET time_zone = "+00:00";
 CREATE TABLE `noticias` (
   `id` int(11) NOT NULL,  
   `titulo` varchar(100) NOT NULL,
-  `conteudo` text NOT NULL
+  `conteudo` text NOT NULL,
+	`comentario_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Índices para tabela noticias
 
 ALTER TABLE `noticias`
   ADD PRIMARY KEY (`id`)
+	ADD KEY `fk_comentario_noticia` (`comentario_id`);
   
 -- AUTO_INCREMENT de tabela noticias
 
 ALTER TABLE `noticias`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+-- Limitadores para a tabela noticias
+
+ALTER TABLE `noticias`
+  ADD CONSTRAINT `fk_comentario_noticia` FOREIGN KEY (`comentario_id`) REFERENCES `comentarios` (`id`);
+COMMIT;
 
 -- Estrutura da tabela comentarios
 
